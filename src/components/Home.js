f7a7234fef67756a250c76e15f3ebf9cdc76c753
mohmedpatel7 <153270796+mohmedpatel7@users.noreply.github.com>
2024-08-13 20:33:47 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./style/style.css";
 
@@ -8,85 +8,79 @@ const Home = ({ setshowGetst, showAlert }) => {
   const navigate = useNavigate();
 
   const [user, setUser] = useState({ name: "" });
-  const fetchUserData = useCallback(async () => {
-    try {
-      const response = await fetch(
-        `https://enotes-backend-3k2a.onrender.com/api/auth/getuser`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "auth-token": localStorage.getItem("token"),
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch user data");
-      }
-
-      const data = await response.json();
-      setUser({ name: data.name });
-    } catch (error) {
-      showAlert("Failed to fetch user data", "danger");
-    }
-  }, [showAlert]);
-
   const [admin, setAdmin] = useState({ Aid: "" });
 
-  const fetchAdminData = useCallback(async () => {
-    try {
-      const response = await fetch(
-        `https://enotes-backend-3k2a.onrender.com/api/admin/getadmin`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "admin-token": localStorage.getItem("admin_token"),
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch admin data");
-      }
-
-      const data = await response.json();
-      setAdmin({ Aid: data.Aid });
-    } catch (error) {
-      showAlert("Failed to fetch admin data", "danger");
-    }
-  }, [showAlert]);
-
   useEffect(() => {
-    fetchUserData();
-    fetchAdminData();
-  }, [fetchUserData, fetchAdminData]);
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(
+          `https://enotes-backend-3k2a.onrender.com/api/auth/getuser`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "auth-token": isUser,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user data");
+        }
+
+        const data = await response.json();
+        setUser({ name: data.name });
+      } catch (error) {
+        showAlert("Failed to fetch user data", "danger");
+      }
+    };
+
+    const fetchAdminData = async () => {
+      try {
+        const response = await fetch(
+          `https://enotes-backend-3k2a.onrender.com/api/admin/getadmin`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "admin-token": isAdmin,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch admin data");
+        }
+
+        const data = await response.json();
+        setAdmin({ Aid: data.Aid });
+      } catch (error) {
+        showAlert("Failed to fetch admin data", "danger");
+      }
+    };
+
+    if (isUser) fetchUserData();
+    if (isAdmin) fetchAdminData();
+  }, [isUser, isAdmin, showAlert]);
 
   return (
     <div>
-      <div className="home-container " style={{ paddingTop: "82px" }}>
+      <div className="home-container" style={{ paddingTop: "82px" }}>
         <header className="hero">
           <h1 className="hero-title">Welcome to ENOTES</h1>
           <p className="hero-subtitle">Your personal note management app</p>
           <br />
-          <>
-            {!localStorage.getItem("token") &&
-              !localStorage.getItem("admin_token") && (
-                <Link
-                  className="btn btn-primary"
-                  onClick={() => setshowGetst(true)}
-                >
-                  Get Started
-                </Link>
-              )}
-            {localStorage.getItem("token") && (
-              <h1 style={{ textTransform: "uppercase" }}>HELLO,{user.name}</h1>
-            )}
-            {localStorage.getItem("admin_token") && (
-              <h1 style={{ textTransform: "uppercase" }}>HELLO,{admin.Aid}</h1>
-            )}
-          </>
+          {!isUser && !isAdmin && (
+            <Link className="btn btn-primary" onClick={() => setshowGetst(true)}>
+              Get Started
+            </Link>
+          )}
+          {isUser && !isAdmin && (
+            <h1 style={{ textTransform: "uppercase" }}>HELLO, {user.name}</h1>
+          )}
+          {!isUser && isAdmin && (
+            <h1 style={{ textTransform: "uppercase" }}>HELLO, {admin.Aid}</h1>
+          )}
         </header>
 
         <section className="features">
@@ -113,11 +107,11 @@ const Home = ({ setshowGetst, showAlert }) => {
           {!isUser && isAdmin && (
             <div className="feature">
               <i
-                class="fa-solid fa-table-columns feature-icon"
+                className="fa-solid fa-table-columns feature-icon"
                 onClick={() => navigate("/Dashboard")}
               ></i>
               <h3>Dashboard</h3>
-              <p>Cheak Dashboard</p>
+              <p>Check Dashboard</p>
             </div>
           )}
           {!isUser && !isAdmin && (
@@ -177,7 +171,7 @@ const Home = ({ setshowGetst, showAlert }) => {
                 onClick={() => navigate("/YourNotes")}
               ></i>
               <h3>View Notes</h3>
-              <p>Cheakout your notes.</p>
+              <p>Check out your notes.</p>
             </div>
           )}
         </section>
