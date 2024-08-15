74976b84ea8bc2ac4e2e9fa2317b2051954a20bc
mohmedpatel7 <153270796+mohmedpatel7@users.noreply.github.com>
2024-08-15 09:47:39 +0530

    updated

--- src/components/User/NoteItem.js
+++ src/components/User/NoteItem.js
@@ -4,14 +4,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "../style/style.css";
 
 export default function NoteItem(props) {
-  const { note, updateNote, showAlert, setshowUpdtModel } = props;
+  const { note, updateNote, showAlert } = props;
 
   const notes = useContext(NoteContext);
   const { del_note } = notes;
 
+  const handleClick = () => {
+    updateNote(note);
+  };
+
   return (
     <div className="col-md-6">
-      <div className="card mb-4" onClick={() => setshowUpdtModel(true)}>
+      <div className="card mb-4" onClick={handleClick}>
         <div className="card-header">{note.title}</div>
         <div className="card-body">
           <h5 className="card-title">{note.tag}</h5>
